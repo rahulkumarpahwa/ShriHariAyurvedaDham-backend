ac12d6c1bcbbe8e18ad985457df5b422edcdd801
Rahul Kumar <rahulkumarpahwa@gmail.com>
2024-04-18 12:09:48 +0530

    fixing path bugs

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import paymentRoutes from "./routes/paymentRoutes.js";
 import cors from "cors";
 import ExpressError from "./utils/ExpressError.js";
-// import path from "path";
-// import { fileURLToPath } from "url";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import { config } from "dotenv";
 config();
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// app.set("views", path.join(__dirname, "views"));
-// app.use(express.static(path.join(__dirname, "/public"))); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/public"))); 
 
 app.use(
   cors({
