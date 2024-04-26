db42948d274d174debcfe5d47db7f4ddc0051385
Rahul Kumar <rahulkumarpahwa@gmail.com>
2024-04-26 20:22:44 +0530

    fix bugs

--- controllers/fetchListings.js
+++ controllers/fetchListings.js
@@ -2,11 +2,11 @@ import { Slide } from "../models/slideModel.js";
 import { Card } from "../models/cardModel.js";
 
 export const fetchSlides = async (req, res) => {
-  const slideData = await Slide.find({});
+  const slideData = await Slide.find();
   res.status(200).json(slideData);
 };
 
 export const fetchCards = async (req, res) => {
-  const cardData = await Card.find({});
+  const cardData = await Card.find();
   res.status(200).json(cardData);
 };
